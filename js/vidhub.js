3d4e2e6174549e347b49bbe0d20eaee1e5fc331a
Bill Li <15819762+billlllie@users.noreply.github.com>
2025-03-23 23:33:22 -0700

    Update UA replace logics

--- js/vidhub.js
+++ js/vidhub.js
@@ -2,9 +2,6 @@ const headers = {
     ...$request.headers
 };
 const ua = headers["User-Agent"];
-if (/MediaCenter.*/.test(ua) || /VidHub/.test(ua)) {
-    console.log("Start to patch");
-    headers["X-Emby-Authorization"] = headers["X-Emby-Authorization"].replace("VidHub_iOS", "Emby_for_iOS");
-    headers["User-Agent"] = "Mozilla/5.0 (iPhone; CPU iPhone OS 18_2_1 like Mac OS X)";
-}
+headers["X-Emby-Authorization"] = headers["X-Emby-Authorization"].replace("VidHub_iOS", "Emby_for_iOS");
+headers["User-Agent"] = "Emby/3.2.32-17.41 (Linux;Android 14) ExoPlayerLib/2.13.2";
 $done({headers});
